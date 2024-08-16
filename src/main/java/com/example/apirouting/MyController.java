f1602b8c494f73990caa669731f71841eedcaadb
prasanthi <prasanthi0221@gmail.com>
2024-08-16 08:18:44 +0000

    solution

---
+++ src/main/java/com/example/apirouting/MyController.java
@@ -0,0 +1,21 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.GetMapping;
+ * import org.springframework.web.bind.annotation.RestController;
+ * 
+ */
+
+// Write your code here.
+package com.example.apirouting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String getMyController() {
+        return "Home Page";
+    }
+}
